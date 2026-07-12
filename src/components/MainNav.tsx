b9bfa06b04e3f0663c48440a8b32a89c4f69f282
Abhinav Tiwari <abhinav13112005@gmail.com>
2026-07-12 23:12:25 +0530

    corrected for deployment

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import UserMenu from "./UserMenu";
 const MainNav = () =>{
 
-    const {loginWithRedirect, isAuthenticated,user} = useAuth0();
+    const {loginWithRedirect, isAuthenticated} = useAuth0();
 return (
     <span className="flex space-x-2 items-center">
         {isAuthenticated ? (
